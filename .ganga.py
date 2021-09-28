a1430f21a42c06b34d0742dc216bc9fd72e6ce41
Yipeng Sun <syp@umd.edu>
2021-09-28 23:56:00 +0200

    Update default lhcb-ntuples-gen project location.

--- .ganga.py
+++ .ganga.py
@@ -36,7 +36,7 @@ OUTPUT_DIR={}
 MIN_NTUPLE_SIZE={}  # in KiB
 
 # User-specific settings, change them according to your environment!
-LNG_PATH=$HOME/eos/src/lhcb-ntuples-gen
+LNG_PATH=$HOME/src/lhcb-ntuples-gen
 YAML_PATH=$LNG_PATH/postprocess/skims/rdx_mc.yml
 BIN_PATH=$LNG_PATH/scripts
 
