e286ce1d93d3079d78faf08b8c7c6f250b6fed40
Yipeng Sun <syp@umd.edu>
2022-02-17 21:15:24 +0100

    Remake a new job for each failed subjob.

--- .ganga.py
+++ .ganga.py
@@ -179,13 +179,18 @@ def remake_uncompleted_job(idx, banned_sites=[
         'LCG.NCBJ.pl',
         'LCG.NIPNE-07.ro',
         'LCG.Beijing.cn'
-]):
-    ds = collect_input_from_uncompleted_job(idx)
+], files_per_job=2):
+    for failed_sj in jobs[idx].subjobs.select(status='failed'):
+        idx_sj = str(failed_sj.fqid).split('.')[1]
+        print(f'Remaking failed subjob {idx_sj}')
 
-    j = jobs(idx).copy()
-    j.inputdata = ds
-    j.backend.settings['BannedSites'] = banned_sites
-    j.submit()
+        j = failed_sj.copy()
+        j.comment = failed_sj.comment + f'--{idx_sj}'
+
+        j.splitter = SplitByFiles(filesPerJob=files_per_job)
+        j.inputdata = failed_sj.inputdata
+        j.backend.settings['BannedSites'] = banned_sites
+        j.submit()
 
 
 def print_job_hadd_filename(init_idx=0):
